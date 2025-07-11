f0e998b00acf5d38f96d8fd9139ffa73d38c0055
mahmoudelbarbari <awad200770@outlook.com>
2025-07-11 18:06:51 +0300

    fixing design user

--- app/(main)/users/[id]/page.js
+++ app/(main)/users/[id]/page.js
@@ -18,17 +18,21 @@ export default async function UserPage({ params }) {
   const user = await res.json();
 
   return (
-    <div className="m-auto">
-      <h1>User Details</h1>
-      <p>
-        <strong>ID:</strong> {user._id}
-      </p>
-      <p>
-        <strong>Name:</strong> {user.name}
-      </p>
-      <p>
-        <strong>Email:</strong> {user.email}
-      </p>
+    <div className="p-6">
+      <h1 className="text-3xl font-bold mb-6 text-center">User Details</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 my-5">
+        <div className=" mx-5 bg-white shadow-md rounded-2xl p-5 border border-gray-100 hover:shadow-lg transition duration-300">
+          <p>
+            <strong>ID:</strong> {user._id}
+          </p>
+          <p>
+            <strong>Name:</strong> {user.name}
+          </p>
+          <p>
+            <strong>Email:</strong> {user.email}
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
